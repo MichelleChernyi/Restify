1d6c14480282e2ebb580c01c366e305bd92b0a84
Devan <devan.srinivasan@mail.utoronto.ca>
2023-03-13 17:53:22 -0700

    startup and run script done?

---
+++ P2/startup.sh
@@ -0,0 +1,20 @@
+# install pip and virtualenv
+sudo apt update
+sudo apt install python3-pip
+sudo apt install python3-virtualenv
+
+# set up virtual env
+virtualenv venv
+source venv/bin/activate
+
+# install project packages
+pip install Django
+pip install Pillow
+pip install djangorestframework
+pip install djangorestframework-simplejwt
+
+# run migrations and data population
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# populate with data?
