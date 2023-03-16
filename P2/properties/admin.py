f4c01e59f1258305c4469baef6dca97b9e14cb21
changem4 <emilychang2002@gmail.com>
2023-03-15 20:51:29 -0400

    admin

--- P2/properties/admin.py
+++ P2/properties/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Property)
+admin.site.register(models.Amenities)
+admin.site.register(models.Availability)
+admin.site.register(models.Image)
