2bdda8987a89dd32a6062919fcce000e8249f91b
Michelle Chernyi <michellechernyi@michelles-air-2.home>
2023-04-12 00:48:54 -0400

    Ffddfdf

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HostIndex from './components/Host'
 import PropertyList from './components/Home';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/host" element={<HostIndex />} />
       <Route path="/" element={<PropertyList />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
    
---
+++ P3/frontend/src/components/Login/index.jsx
@@ -0,0 +1,120 @@
+import React, { Component, useState} from 'react';
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import axios from 'axios';
+// import './style.css';
+import {Link, useParams, Navigate, useNavigate, Route} from "react-router-dom";
+
+
+
+// States for registration
+// const [firstName, setFirstName] = useState('');
+// const [lastName, setLastName] = useState('');
+// const [email, setEmail] = useState('');
+// const [password, setPassword] = useState('');
+// const [phone, setPhone] = useState('');
+// const [avatar, setAvatar] = useState('');
+// const [error, setError] = useState(false);
+
+// avatar:"",
+
+const Nav = () => {
+  const navigate = useNavigate();
+  return navigate('/')
+};
+class Login extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        email: "", password: "", email_error: "", password_error: "", 
+        
+      };
+    }
+    onChange = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+    logIn = e => {
+      e.preventDefault();
+       
+
+      axios.post("http://127.0.0.1:8000/api/token/", {email: this.state.email, password: this.state.password})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+
+        window.location.assign("http://localhost:3000/");
+      })
+      .catch(err => {
+        console.log(err.response.data)
+        console.log(err.response.data)
+        this.setState({ email_error: "" }); 
+        this.setState({ password_error:"" });
+        for (var key in err.response.data) {
+            console.log("Key:" + key);
+          console.log("Value:" + err.response.data[key]);
+          if( key === 'email'){
+            this.setState({ email_error: err.response.data[key][0] }); 
+          }
+          if(key === 'detail'){
+            this.setState({ password_error: err.response.data[key] }); 
+          }
+
+          if( key === 'password'){
+            // this.setState({ email_error: err.response.data[key]});
+            this.setState({ password_error: err.response.data[key][0] }); 
+          }
+
+          
+        }
+
+
+
+      
+      
+    });
+    };
+
+    render() {
+      return (
+        <>
+        <div className="page">
+          <main>
+        <div className="card w-500 log-stuff register">
+        <div className="card-body logs">
+        <h5 className="card-title">Welcome to Restify</h5>
+        <Form onSubmit={this.logIn} noValidate>
+          <FormGroup>
+            <Label for="email">Email:</Label>
+            <Input
+              type="email"
+              name="email"
+              onChange={this.onChange}
+              value={this.state.email}
+            />
+             <p>{this.state.email_error}</p>
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              onChange={this.onChange}
+              value={this.state.password}
+            />
+             <p>{this.state.password_error}</p>
+          </FormGroup>
+          <Button className="btn btn-primary sign-btn" >Sign Up</Button>
+        </Form>
+       <Link to='/register'>Don't have an account yet?</Link>
+            
+        </div>
+        </div>
+        </main>
+        </div>
+        </>
+      );
+    }
+
+}
+
+
+export default Login
