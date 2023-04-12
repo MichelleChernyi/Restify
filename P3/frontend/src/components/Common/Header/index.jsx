0f9b10cd40b32a69152c8c8083c75c1149fbd7a8
Michelle Chernyi <michellechernyi@michelles-air-2.home>
2023-04-12 14:02:04 -0400

    Fdff

--- P3/frontend/src/components/Common/Header/index.jsx
+++ P3/frontend/src/components/Common/Header/index.jsx
@@ -81,6 +81,15 @@ function Header(props) {
     window.location.assign("http://localhost:3000/notifications/");
     
   }
+  const reserve = () => {
+    window.location.assign("http://localhost:3000/reservations/");
+    
+  }
+
+  const host = () => {
+    window.location.assign("http://localhost:3000/host/");
+    
+  }
   
   return (
         <>
@@ -130,9 +139,9 @@ function Header(props) {
                     <i className="bi bi-person-fill m-4 w-150 fs-3"></i>
                   </a>
                   <ul className="dropdown-menu dropdown-menu-end me-1">
-                    <li><a className="dropdown-item" href="add-new-property.html">My Properties</a></li>
+                    <li><a className="dropdown-item" onClick={host}>My Properties</a></li>
                   <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="no-reservations.html">My Reservations</a></li>
+                  <li><a className="dropdown-item" onClick={reserve}>My Reservations</a></li>
                   <li><hr className="dropdown-divider"/></li>
                   <li><a className="dropdown-item" onClick={profile}>My Profile</a></li>
                   <li><hr className="dropdown-divider"/></li>
