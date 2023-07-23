25fd7b3a9726a0c2321a0da543755c55364423da
Michelle Chernyi <81598850+MichelleChernyi@users.noreply.github.com>
2023-07-23 16:20:54 -0400

    Delete P2 directory

--- P2/properties/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-admin.site.register(models.Property)
-admin.site.register(models.Amenities)
-admin.site.register(models.Availability)
-admin.site.register(models.Image)
-admin.site.register(models.PropertyComment)
-admin.site.register(models.Reservation)
